e6647aa2da0b44d2df1e4eba134ec78eef824f84
Akshat <akshath.centaurus@gmail.com>
2023-01-16 14:04:39 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const { Configuration, OpenAIApi } = require("openai");
+const express=require('express')
+const bodyParser=require('body-parser')
+const cors=require('cors')
+
+
+
+const configuration = new Configuration({
+    organization: "org-55d8hQ0x4Pkfu666gLsaXEdi",
+    apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+
+
+const app=express()
+app.use(bodyParser.json())
+app.use(cors())
+const port=3080 
+
+app.post('/',async(req,res)=>{
+    const {message}=req.body;
+    console.log(message,"message")
+    
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: `${message}`,
+        max_tokens: 100,
+        temperature: 0.5,
+      });
+    
+      res.json({
+        message:response.data.choices[0].text
+        
+      })
+ });
+
+ app.get('/models',async(req,res)=>{
+    const response = await openai.listEngines();
+    console.log(response.data.data)
+    res.json({
+        models: response.data.data
+    })
+    
+ });
+
+ app.listen(port,()=>{
+    console.log(`Example app listening at http://localhost:${port}`)
+ });
